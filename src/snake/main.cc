8f4dd44c5d8b819f03e029131ad308bf3576d42e
mllimo <ar.guijarro.contreras@gmail.com>
2020-11-22 23:44:19 +0000

    cambiando main

--- src/snake/main.cc
+++ src/snake/main.cc
@@ -1,6 +1,19 @@
-#include <iostream>
+#include "../include/SFML/Audio.hpp"
+#include "../include/SFML/Graphics.hpp"
 
 int main() {
-  std::cout << "Hola Mundo!" << std::endl;
+  sf::Window window(sf::VideoMode(800, 600), "My window");
+
+  // run the program as long as the window is open
+  while (window.isOpen()) {
+    // check all the window's events that were triggered since the last iteration of the loop
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      // "close requested" event: we close the window
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+  }
+
   return 0;
 }
