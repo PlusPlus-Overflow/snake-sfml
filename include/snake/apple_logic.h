3c3eab4509747020d1d04ded68bad6c49610b11d
alu0101100654 <alu0101100654@ull.edu.es>
2020-11-23 00:10:23 +0000

    Clase AppleLogic creada

---
+++ include/snake/apple_logic.h
@@ -0,0 +1,14 @@
+#ifndef __APPLE_LOGIC_HPP__
+#define __APPLE_LOGIC_HPP__
+
+#include"ObjectLogic.h"
+
+namespace snk {
+
+  class AppleLogic : public ObjectLogic {
+    using ObjectLogic::ObjectLogic;
+  };
+
+}
+
+#endif
