1c1d41aa30fbe7f001b64de706bea74deea77ba8
mllimo <ar.guijarro.contreras@gmail.com>
2020-11-23 00:11:21 +0000

    Generando cabecera de la clase BoardController

---
+++ include/snake/board_controller.h
@@ -0,0 +1,21 @@
+#ifndef BOARD_CONTROLLER_H_
+#define BOARD_CONTROLLER_H_
+
+#include "board-graphic.h"
+#include "board-logic.h"
+
+namespace snk {
+
+class BoardController {
+ public:
+  BoardController(const BoardLogic& board_logic, const BoardGraphic& board_graphic);
+  ~BoardController();
+
+ private:
+  BoardLogic board_logic_;
+  BoardGraphic board_graphic;
+};
+
+}  // namespace snk
+
+#endif
