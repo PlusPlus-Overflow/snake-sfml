feebbb34a2288accd66554c62b47f13c3fc8a19f
mllimo <ar.guijarro.contreras@gmail.com>
2020-11-23 02:10:15 +0000

    arreglando path

--- src/snake/main.cc
+++ src/snake/main.cc
@@ -1,19 +1,13 @@
-#include "../include/SFML/Audio.hpp"
-#include "../include/SFML/Graphics.hpp"
+#include "../../include/snake/apple_logic.h"
+#include "../../include/snake/board_controller.h"
+#include "../../include/snake/board_graphic.h"
+#include "../../include/snake/board_logic.h"
+#include "../../include/snake/object_controller.h"
+#include "../../include/snake/object_graphic.h"
+#include "../../include/snake/object_logic.h"
+#include "../../include/snake/snake_head_logic.h"
 
 int main() {
-  sf::Window window(sf::VideoMode(800, 600), "My window");
-
-  // run the program as long as the window is open
-  while (window.isOpen()) {
-    // check all the window's events that were triggered since the last iteration of the loop
-    sf::Event event;
-    while (window.pollEvent(event)) {
-      // "close requested" event: we close the window
-      if (event.type == sf::Event::Closed)
-        window.close();
-    }
-  }
 
   return 0;
 }
